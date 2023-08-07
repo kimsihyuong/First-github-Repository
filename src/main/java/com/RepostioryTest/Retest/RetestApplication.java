cf891c4b67eba657194204a1d2bfd0ef6f5ddc46
kimsihyoung <rjswmtlgud@naver.com>
2023-08-07 19:18:38 +0900

    Main

---
+++ src/main/java/com/RepostioryTest/Retest/RetestApplication.java
@@ -0,0 +1,13 @@
+package com.RepostioryTest.Retest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetestApplication.class, args);
+	}
+
+}
