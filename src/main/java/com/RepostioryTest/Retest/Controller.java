b79ae6b21265fb68c5bacc8cf92008b7c1a2768e
kimsihyoung <rjswmtlgud@naver.com>
2023-08-07 21:00:56 +0900

    컨트롤러 제작 수정

--- src/main/java/com/RepostioryTest/Retest/Controller.java
+++ src/main/java/com/RepostioryTest/Retest/Controller.java
@@ -1,2 +1,12 @@
-package com.RepostioryTest.Retest;public class Controller {
+package com.RepostioryTest.Retest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application!";
+    }
 }
